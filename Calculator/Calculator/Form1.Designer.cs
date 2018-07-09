e1f883b32f288e6ee9c2183e5338c1df80cc3438
jebbyk <zaicev9797@gmail.com>
2018-07-09 13:58:02 +0600

    rename buttons and fields. Add functionality for buttons

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -28,90 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Number1 = new System.Windows.Forms.TextBox();
-            this.Number2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Number1Field = new System.Windows.Forms.TextBox();
+            this.Number2Field = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMult = new System.Windows.Forms.Button();
+            this.buttonDevide = new System.Windows.Forms.Button();
+            this.ResultField = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Number1
+            // Number1Field
             // 
-            this.Number1.Location = new System.Drawing.Point(12, 25);
-            this.Number1.Name = "Number1";
-            this.Number1.Size = new System.Drawing.Size(150, 20);
-            this.Number1.TabIndex = 0;
-            this.Number1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.Number1Field.Location = new System.Drawing.Point(12, 25);
+            this.Number1Field.Name = "Number1Field";
+            this.Number1Field.Size = new System.Drawing.Size(150, 20);
+            this.Number1Field.TabIndex = 0;
+            this.Number1Field.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // Number2
+            // Number2Field
             // 
-            this.Number2.Location = new System.Drawing.Point(168, 25);
-            this.Number2.Name = "Number2";
-            this.Number2.Size = new System.Drawing.Size(150, 20);
-            this.Number2.TabIndex = 1;
-            this.Number2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.Number2Field.Location = new System.Drawing.Point(168, 25);
+            this.Number2Field.Name = "Number2Field";
+            this.Number2Field.Size = new System.Drawing.Size(150, 20);
+            this.Number2Field.TabIndex = 1;
+            this.Number2Field.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // button1
+            // buttonPlus
             // 
-            this.button1.Location = new System.Drawing.Point(12, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(46, 41);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonPlus.Location = new System.Drawing.Point(12, 51);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(46, 41);
+            this.buttonPlus.TabIndex = 2;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlusClick);
             // 
-            // button2
+            // buttonMinus
             // 
-            this.button2.Location = new System.Drawing.Point(64, 51);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(46, 41);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "-";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.buttonMinus.Location = new System.Drawing.Point(64, 51);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(46, 41);
+            this.buttonMinus.TabIndex = 3;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinusClick);
             // 
-            // button3
+            // buttonMult
             // 
-            this.button3.Location = new System.Drawing.Point(116, 51);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(46, 41);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "*";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.buttonMult.Location = new System.Drawing.Point(116, 51);
+            this.buttonMult.Name = "buttonMult";
+            this.buttonMult.Size = new System.Drawing.Size(46, 41);
+            this.buttonMult.TabIndex = 4;
+            this.buttonMult.Text = "*";
+            this.buttonMult.UseVisualStyleBackColor = true;
+            this.buttonMult.Click += new System.EventHandler(this.buttonMultClick);
             // 
-            // button4
+            // buttonDevide
             // 
-            this.button4.Location = new System.Drawing.Point(168, 51);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(46, 41);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "/";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.buttonDevide.Location = new System.Drawing.Point(168, 51);
+            this.buttonDevide.Name = "buttonDevide";
+            this.buttonDevide.Size = new System.Drawing.Size(46, 41);
+            this.buttonDevide.TabIndex = 5;
+            this.buttonDevide.Text = "/";
+            this.buttonDevide.UseVisualStyleBackColor = true;
+            this.buttonDevide.Click += new System.EventHandler(this.buttonDevideClick);
             // 
-            // textBox1
+            // ResultField
             // 
-            this.textBox1.Location = new System.Drawing.Point(324, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 20);
-            this.textBox1.TabIndex = 6;
+            this.ResultField.Location = new System.Drawing.Point(324, 25);
+            this.ResultField.Name = "ResultField";
+            this.ResultField.Size = new System.Drawing.Size(150, 20);
+            this.ResultField.TabIndex = 6;
+            this.ResultField.TextChanged += new System.EventHandler(this.Result_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.Number2);
-            this.Controls.Add(this.Number1);
+            this.Controls.Add(this.ResultField);
+            this.Controls.Add(this.buttonDevide);
+            this.Controls.Add(this.buttonMult);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.Number2Field);
+            this.Controls.Add(this.Number1Field);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -121,13 +122,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox Number1;
-        private System.Windows.Forms.TextBox Number2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Number1Field;
+        private System.Windows.Forms.TextBox Number2Field;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMult;
+        private System.Windows.Forms.Button buttonDevide;
+        private System.Windows.Forms.TextBox ResultField;
     }
 }
 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
+
+    
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,10 +20,6 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -43,6 +42,46 @@ namespace Calculator
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlusClick(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(Number1Field.Text);
+            double number2 = Convert.ToDouble(Number2Field.Text);
+            double result = number1 + number2;
+            ResultField.Text = result.ToString();
+        }
+
+        private void buttonMinusClick(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(Number1Field.Text);
+            double number2 = Convert.ToDouble(Number2Field.Text);
+            double result = number1 - number2;
+            ResultField.Text = result.ToString();
+        }
+
+        private void buttonMultClick(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(Number1Field.Text);
+            double number2 = Convert.ToDouble(Number2Field.Text);
+            double result = number1 * number2;
+            ResultField.Text = result.ToString();
+        }
+
+        private void buttonDevideClick(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(Number1Field.Text);
+            double number2 = Convert.ToDouble(Number2Field.Text);
+            double result = number1 / number2;
+            ResultField.Text = result.ToString();
+        }
+
+
+
+
+        private void Result_TextChanged(object sender, EventArgs e)
         {
 
         }
