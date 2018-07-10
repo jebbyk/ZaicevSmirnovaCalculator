2f0f8739cd194afec8ff6c9d9f9f22b91ce98f3e
jebbyk <zaicev9797@gmail.com>
2018-07-10 14:56:36 +0600

    added factory tests

---
+++ Calculator/Calculator.Tests/oneOperandFunctionalityTests/OneOperandFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.oneOperandFunctionality;
+using Calculator.twoOperandsFunctionality;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Calculator.Tests.oneOperandFunctionalityTests
+{
+    class OneOperandFactoryTests
+    {
+        [TestCase("Sin", typeof(SinusCalculator))]
+        [TestCase("Cos", typeof(CosinusCalculator))]
+        [TestCase("Sqrt", typeof(SqrtCalculator))]
+        [TestCase("Sqr", typeof(SquareCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentsCalculatorFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/twoOperandsFunctionalityTests/TwoOperndsFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.twoOperandsFunctionality;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Calculator.Tests.twoOperandsFunctionalityTests
+{
+    public class TwoOperndsFactoryTests
+    {
+        [TestCase("+", typeof(AddittionCalculator))]
+        [TestCase("-", typeof(SubstractionCalculator))]
+        [TestCase("*", typeof(MultiplyCalculator))]
+        [TestCase("/", typeof(DivisionCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsCalculatorFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
