769b1c288aec025846b3fe1cfe21ee9c7f014409
ekaterinasmirnova1 <smirnova190998@mail.ru>
2018-07-12 12:45:37 +0600

    added web functionality

---
+++ Calculator/CalculatorWebInterfase/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.twoOperandsFunctionality;
+
+namespace CalculatorWebInterfase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<SelectListItem> operations = new List<SelectListItem>()
+        {
+            new SelectListItem()
+            {
+                Text = "+", Value = "+"
+            },new SelectListItem()
+            {
+                Text = "-", Value = "-"
+            },new SelectListItem()
+            {
+                Text = "*", Value = "*"
+            },new SelectListItem()
+            {
+                Text = "/", Value = "/"
+            },new SelectListItem()
+            {
+                Text = "x^y", Value = "x^y"
+            }
+        };
+
+        [HttpPost]
+        public ActionResult Index(double firstNumber, double secondNumber, string operation)
+        {
+            ITwoArgumentsCalculator calculator = TwoArgumentsCalculatorFactory.CreateCalculator(operation);
+            ViewBag.result = calculator.Calculate(firstNumber, secondNumber);
+            ViewBag.operations = operations;
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.operations = operations;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Calculator/CalculatorWebInterfase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CalculatorWebInterfase.MvcApplication" Language="C#" %>
